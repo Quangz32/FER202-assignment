626e604c86a154d716de901f58923c17fc2968c9
Quangz <quang@kobizo.de>
2024-07-12 19:14:07 +0700

    why choose us

--- src/home/Home.js
+++ src/home/Home.js
@@ -71,7 +71,7 @@ export default function Home() {
       {/* ------- Hero End --------*/}
 
       {/* ----- Product Start ----- */}
-      <div className="mt-5">
+      <div className="mt-5 mb-5">
         <Container>
           <Row>
             <Col md={12} lg={3}>
@@ -102,17 +102,57 @@ export default function Home() {
       {/* ----- Product end ----- */}
 
       {/* ----- Why choose Start ----- */}
-      <div>
+      <div className="mt-5 mb-5">
         <Container>
           <Row>
             <Col sm={12} md={6}>
               <h2>Why choose us</h2>
-              <p>
+              <p className="text-secondary">
                 Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
                 vulputate velit imperdiet dolor tempor tristique.
               </p>
+              <Row>
+                <Col>
+                  <Row className="mb-3">
+                    <i className="bi bi-truck fs-3"></i>
+                    <h6>Fast &amp; Free Shipping</h6>
+                    <p className="text-secondary">
+                      Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
+                      vulputate.
+                    </p>
+                  </Row>
+                  <Row>
+                    <i className="bi bi-cart-check fs-3"></i>
+                    <h6>Easy to shop</h6>
+                    <p className="text-secondary">
+                      Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
+                      vulputate.
+                    </p>
+                  </Row>
+                </Col>
+                <Col>
+                  <Row className="mb-3">
+                    <i className="bi bi-headset fs-3"></i>
+                    <h6>24/7 Support</h6>
+                    <p className="text-secondary">
+                      Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
+                      vulputate.
+                    </p>
+                  </Row>
+                  <Row>
+                    <i className="bi bi-arrow-repeat fs-3"></i>
+                    <h6>Return easily</h6>
+                    <p className="text-secondary">
+                      Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
+                      vulputate.
+                    </p>
+                  </Row>
+                </Col>
+              </Row>
             </Col>
-            <Col sm={12} md={6}></Col>
+            <Col sm={12} md={6} className="px-5 ">
+              <img className="img-fluid rounded-5" src="images/why-choose-us-img.jpg"></img>
+            </Col>
           </Row>
         </Container>
       </div>
